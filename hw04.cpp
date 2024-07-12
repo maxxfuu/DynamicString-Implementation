8af37b1e5d0488384868deb497886fb8bdfa293c
Max Fu <mfu27@ucmerced.edu>
2024-07-12 01:17:01 -0700

    segmentation fault with move constructor

--- hw04.cpp
+++ hw04.cpp
@@ -1,7 +1,6 @@
 
 // write more test functions here
 #include "hw04.h"
-#include <fstream>
 
 // Write all these testing functions and add more of your own
 // follow the example and write a function to test each method.
@@ -14,7 +13,13 @@ int main() {
         std::cout << "Default Constructor" << std::endl; 
 
         StringType str1("hello");
-        std::cout << "Constructor using C-string:" << str1 << std::endl; 
+        std::cout << "Constructor using C-string:" << str1 << std::endl;  
+
+        StringType str2 = str1;
+        std::cout << "Copy constructor: " << str2 << std::endl;
+
+        StringType str3 = std::move(str2);
+        std::cout << "Move constructor: " << str3 << std::endl;
 
     
     } catch (const std::exception& e) {
